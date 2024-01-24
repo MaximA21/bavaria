4e59908dab71effe5fc2c2c14d2a992a93bc2a3e
Maxim <118841284+MaximA21@users.noreply.github.com>
2024-01-24 18:26:38 +0100

    fix: image loading

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ function App() {
                         <Image
                             alt="Card background"
                             className="object-cover rounded-xl m-auto"
-                            src="../assets/images/featured/featured-2.jpg"
+                            src="/assets/images/featured/featured-2.jpg"
                             removeWrapper={true} width={170}
                         />
                     </CardBody>
@@ -130,7 +130,7 @@ function App() {
                         <Image
                             alt="Card background"
                             className="object-cover rounded-xl m-auto"
-                            src="../assets/images/featured/bavarianHat.png"
+                            src="/assets/images/featured/bavarianHat.png"
                             removeWrapper={true} width={170}
                         />
                     </CardBody>
